96c44249675c36a2bcc82e72779dc2030922a563
Angga Putra <anggapratap@gmail.com>
2020-02-07 23:39:41 +0700

    create input component

---
+++ src/components/Input/index.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+const Input = styled.input`
+    width: 295px;
+    height: 48px;
+
+    background: #FFFFFF;
+    border: 1px solid #FC6F20;
+    border-radius: 24px;
+    box-sizing: border-box;
+
+    font-size: 16px;
+    margin: 10px;
+    padding: 20px;
+
+    :focus {
+        outline: none;
+    }
+`;
+
+export default Input;
--- src/containers/Register/index.js
+++ src/containers/Register/index.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import logo from 'assets/images/timy-logo.png';
+import Input from 'components/Input';
 
 class Register extends Component {
 	render() {
 		return (
-			<div className="Register">
-				<h1>Timy Dashboard</h1>
+			<div className="centered">
+				<img src={logo} alt="timy-logo" />
+				<div className="logo-tagline">Remote working<br/>time tracker</div>
+				<Input 
+					placeholder="Email"
+				/>
+				<Input 
+					placeholder="Name"
+				/>
+				<Input 
+					placeholder="Password"
+				/>
+				<Input 
+					placeholder="Confirm password"
+				/>
 			</div>
 		);
 	}

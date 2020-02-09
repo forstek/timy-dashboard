c7419878cfc0971632cd21ce307cf6452b012748
Angga Putra <anggapratap@gmail.com>
2020-02-09 23:10:42 +0700

    style: add semicolon at src/index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './styles/index.css'
-import Register from 'containers/Register'
-import * as serviceWorker from './serviceWorker'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+import Register from 'containers/Register';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Register />, document.getElementById('root'))
 

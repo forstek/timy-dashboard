867e66ec61c4eb7d4ee9ec9c3ee9291c8898e269
Angga Putra <anggapratap@gmail.com>
2020-02-09 22:58:16 +0700

    chore: set register container to initial setup

--- src/containers/Register/index.js
+++ src/containers/Register/index.js
@@ -1,21 +1,9 @@
 import React, { Component } from 'react'
-import Button from 'components/Button'
-import Google from 'assets/images/google-logo.png'
 
 class Register extends Component {
   render () {
     return (
-      <div className='Register'>
-        <Button
-          text='Sign up'
-        />
-        <Button
-          src={Google}
-          background='#FFFFFF'
-          color='#707070'
-          text='Sign in'
-        />
-      </div>
+      <h1>Timy Dashboard</h1>
     )
   }
 }

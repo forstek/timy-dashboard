e91c7f7f4215732d57bc88e44fb254943a9954f4
Angga Putra <anggapratap@gmail.com>
2020-02-10 17:38:12 +0700

    chore: change index.js to initial, to avoid conflict

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './styles/index.css'
-import Login from 'containers/Login'
+import Register from 'containers/Register'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<Login />, document.getElementById('root'))
+ReactDOM.render(<Register />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
